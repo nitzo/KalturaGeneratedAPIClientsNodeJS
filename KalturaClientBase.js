8b9904f214dd49af9de93cc18519b270774c7502
Nitzan Bar <nitzanbar@gmail.com>
2020-04-19 18:32:05 +0300

    Update KalturaClientBase.js

--- KalturaClientBase.js
+++ KalturaClientBase.js
@@ -310,8 +310,7 @@ class RequestBuilder extends kaltura.VolatileRequestData {
 		else {
 			body = jsonBody;
 		}
-
-		options.headers['Content-Length'] = body.length;
+		
 		options.body = body;
 
 		client.request(options, (err, response, data) => {
